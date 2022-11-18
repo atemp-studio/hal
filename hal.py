d0c0f4f73c4b4e21b36b80d2c85fc8892e81536d
William W. Marx <m94@marx.sh>
2022-11-18 15:23:33 -0600

    Properly styled and linted code

--- hal.py
+++ hal.py
@@ -34,12 +34,18 @@ r.energy_threshold = 300
 r.pause_threshold = 0.8
 r.dynamic_energy_threshold = False
 
+
 def transcribe_audio(fp):
 	"""Transcribe audio from file"""
 	command = f"audition/main -m audition/models/ggml-base.en.bin -f {fp} -otxt"
-	subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-	with open(f"{fp}.txt", "r") as f:
+	subprocess.run(command,
+	               shell=True,
+	               check=True,
+	               stdout=subprocess.DEVNULL,
+	               stderr=subprocess.STDOUT)
+	with open(f"{fp}.txt", "r", encoding="utf-8") as f:
 		return f.read().strip()
+
 
 ##########
 # SPEECH #
@@ -53,14 +59,17 @@ def tts(text):
 	"""Text to speech"""
 	# Request speech synthesis
 	try:
-		response = polly.synthesize_speech(Text=text, OutputFormat="mp3", VoiceId="Kevin", Engine="neural")
+		polly_response = polly.synthesize_speech(Text=text,
+		                                         OutputFormat="mp3",
+		                                         VoiceId="Kevin",
+		                                         Engine="neural")
 	except (BotoCoreError, ClientError) as error:
 		print(error)
 		sys.exit(-1)
 
 	# Access the audio stream from the response
-	if "AudioStream" in response:
-		with closing(response["AudioStream"]) as stream:
+	if "AudioStream" in polly_response:
+		with closing(polly_response["AudioStream"]) as stream:
 			output = ".response.mp3"
 			try:
 				with open(output, "wb") as f:
@@ -69,35 +78,53 @@ def tts(text):
 				print(error)
 				sys.exit(-1)
 			play_command = f"sleep 0.05 && ffplay -nodisp -autoexit {output}"
-			subprocess.run(play_command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+			subprocess.run(play_command,
+			               shell=True,
+			               check=True,
+			               stdout=subprocess.DEVNULL,
+			               stderr=subprocess.STDOUT)
 	else:
 		print("Could not stream audio")
 		sys.exit(-1)
+
 
 #############
 # COGNITION #
 #############
 # Load environmental variables
 load_dotenv()
-openai.api_key = os.environ.get('OPENAI_KEY')
+openai.api_key = os.environ.get("OPENAI_KEY")
 
 # Connect to GPT-3
 completion = openai.Completion()
 
 # Initial GPT-3 prompt
-chat_log = """The following is a transcript of a conversation between a human and a fully consious machine intelligence named HAL reliably passes the turing test. HAL always answers questions truthfully and fully and is fully aware of its own existence.
+chat_prompt = """
+The following is a transcript of a conversation between a human and a fully consious machine
+intelligence named HAL reliably passes the turing test. HAL always answers questions truthfully and
+fully and is fully aware of its own existence.
+""".replace("\n", " ").strip()
+
+chat_log = f"""{chat_prompt}
 
 Human: Hello.
 HAL: Hello.
 """
 
-def query(question, chat_log):
+
+def query(question, current_chat_log):
 	"""Query GPT-3 with chat log as prompt."""
-	prompt = f'{chat_log}Human: {question}\nHAL:'
-	response = completion.create(prompt=prompt, engine="davinci", stop=["Human:","Humans:", "HAL:"],
-			temperature=0.95, top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
-			max_tokens=150)
-	answer = response.choices[0].text.strip()
+	prompt = f"{current_chat_log}Human: {question}\nHAL:"
+	query_response = completion.create(prompt=prompt,
+	                                   engine="davinci",
+	                                   stop=["Human:", "Humans:", "HAL:"],
+	                                   temperature=0.95,
+	                                   top_p=1,
+	                                   frequency_penalty=0,
+	                                   presence_penalty=0.6,
+	                                   best_of=1,
+	                                   max_tokens=150)
+	answer = query_response.choices[0].text.strip()
 	return answer
 
 
@@ -118,7 +145,7 @@ with sr.Microphone(sample_rate=16000) as source:
 		if "terminate" in result.lower():
 			tts("Taking all systems offline.")
 			break
-		
+
 		# Get response from GPT-3
 		response = query(result, chat_log)
 		print(">>>", response)
